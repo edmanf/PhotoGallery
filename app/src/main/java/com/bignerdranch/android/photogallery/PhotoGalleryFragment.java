c8e5ef5ffaebe278b37dd16a65d1f67e6f3cbc53
Edman Feng <edmanfeng@hotmail.com>
2017-04-01 02:57:31 -0700

    23.1: done

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
@@ -0,0 +1,12 @@
+package com.bignerdranch.android.photogallery;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by t7500 on 4/1/2017.
+ */
+
+public class PhotoGalleryFragment extends Fragment {
+
+
+}
