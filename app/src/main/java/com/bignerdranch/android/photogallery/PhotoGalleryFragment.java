a43fbb666d6ca47246920a3e4d060b7ca6482115
Edman Feng <edmanfeng@hotmail.com>
2017-04-12 02:35:08 -0700

    23.5: Added FetchItemsTask

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
@@ -1,19 +1,25 @@
 package com.bignerdranch.android.photogallery;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.io.IOException;
 
 /**
  * Created by t7500 on 4/1/2017.
  */
 
 public class PhotoGalleryFragment extends Fragment {
+
+    private static final String TAG = "PhotoGalleryFragment";
 
     private RecyclerView mPhotoRecyclerView;
 
@@ -38,5 +44,19 @@ public class PhotoGalleryFragment extends Fragment {
                 .findViewById(R.id.fragment_photo_gallery_recycler_view);
         mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
         return v;
+    }
+
+    private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                String result = new FlickerFetchr()
+                        .getUrlString("https://www.bignerdranch.com");
+                Log.i(TAG, "Fetched content of URL: " + result);
+            } catch (IOException e) {
+                Log.e(TAG, "Failed to fetch URL: ", e);
+            }
+            return null;
+        }
     }
 }
